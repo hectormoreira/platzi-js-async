1577f03d0fa9942624319fb6bc85a5356c85f376
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-03-07 14:29:01 -0500

    callback hell

---
+++ src/callback/challenge.js
@@ -0,0 +1,38 @@
+let XMLHttpRquest = require("xmlhttprequest").XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/'
+
+function fetchData(url_api, callback) {
+  let xhttp = new XMLHttpRquest();
+  xhttp.open("GET", url_api, true); //3cer valor es async
+
+  /*onreadystatechange: los estados que puede tener son:
+        estado 0: inicializado
+        estado 1: cargando
+        estado 2: ya se cargó
+        estado 3: ya hay información
+        estado 4: solicitud completa */
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error " + url_api);
+        return callback(error, null)
+      }
+    }
+  };
+  xhttp.send();
+}
+
+fetchData(API, function(error1, data1){
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function (error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function (error3, data3){
+            if (error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    });
+})
